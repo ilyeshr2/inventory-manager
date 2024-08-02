2c3f8d0118a7cc233aed21650df4061599879e50
ilyeshr2 <128542440+ilyeshr2@users.noreply.github.com>
2024-08-02 08:36:53 -0400

    Sidebar Update

--- client/src/app/(components)/Sidebar/index.tsx
+++ client/src/app/(components)/Sidebar/index.tsx
@@ -1,10 +1,33 @@
+'use client'
+import { Menu } from 'lucide-react'
 import React from 'react'
 
 
 
 const Sidebar = () => {
   return (
-    <div>Sidebarx</div>
+    <div className='light'>
+      <div
+         className={"flex gap-3 justify-between md:justify-normal items-center pt-8"}>
+          <div>logo</div>
+          <h1 className='font-extrabold text-2xl'>IHSTOCK</h1>
+
+          <button
+            className='md:hidden px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100'
+            onClick={() => {}}
+          >
+            <Menu className='w-4 h-4' />
+          </button>
+      </div>
+      {/* links */}
+      <div className='flex-grow mt-8'>{/* links */}</div>
+
+      {/* footer */}
+      <div>
+        <p className='text-center text-xs text-gray-500'>© 2021 IHSTOCK</p>
+      </div>
+    </div>
+    
   )
 }
 
