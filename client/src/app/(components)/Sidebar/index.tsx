4e9d10bd6990b76c7c488de0d4c6488e9c3d64ba
ilyeshr2 <128542440+ilyeshr2@users.noreply.github.com>
2024-08-13 14:21:30 -0400

    Update index.tsx

--- client/src/app/(components)/Sidebar/index.tsx
+++ client/src/app/(components)/Sidebar/index.tsx
@@ -16,7 +16,7 @@ const Sidebar = () => {
             className='md:hidden px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100'
             onClick={() => {}}
           >
-            <Menu className='w-4 h-4' />
+          <Menu className='w-4 h-4' />
           </button>
       </div>
       {/* links */}
